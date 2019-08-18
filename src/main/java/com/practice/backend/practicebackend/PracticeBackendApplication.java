0d0980e54178847f2e14f3076517308e648e5dc4
subhra biswas <subhrab2@gmail.com>
2019-08-18 23:00:27 +0530

    Initial spring boot project

---
+++ src/main/java/com/practice/backend/practicebackend/PracticeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.practice.backend.practicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/backend/practicebackend/PracticeBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.practice.backend.practicebackend;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class PracticeBackendApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
